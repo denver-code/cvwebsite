6524c69ae48e3b0e40f08c4e42ace8a79fc3d5ca
denver-code <csigorek@gmail.com>
2023-05-18 17:58:23 +0100

    intro text refactoring

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,8 +6,7 @@ function Intro() {
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Ihor Savenko</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & ICR Student</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm an indie software developer and building my
-            version of the digital world one step at a time. All coding projects
+            I'm an indie software developer with a lot of enthusiasm. All projects
             are built to solving real-life problems with code.
             <br />
          </p>
